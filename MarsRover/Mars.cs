554109b7b6abae6fbdcbb0d57a9133e233258e9d
Leerling <Leerling@IBP-13>
2019-02-05 11:55:23 +0100

    Add project files.

---
+++ MarsRover/Mars.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class Mars
+    {
+      public int grootteX = 40;
+        public int grootteY = 20;
+        bool[,] arrayrotsen = new bool[40,20];
+        public int posRoverRotsX, posRoverRotsY;
+
+
+
+        public Mars()
+        {
+            RotsenGenereren();
+            RotsenTonen();
+            
+        }
+
+        
+
+
+        //Maakt een omgeving waarin je kan bewegen (in de console)
+        public void toonMars()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(0, 0);
+            Console.Write("╔");
+            for (int i = 1; i < grootteX; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("═");
+            }
+            //hello me old chum
+            Console.Write("╗");
+            for (int i = 1; i < grootteY; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("║");
+            }
+            //im gnot a gnelf
+            Console.SetCursorPosition(0, grootteY);
+            Console.Write("╚");
+            for (int i = 1; i < grootteY; i++)
+            {
+                Console.SetCursorPosition(grootteX, i);
+                Console.Write("║");
+            }
+            //im gnot a gnoblin
+            Console.SetCursorPosition(grootteX, grootteY);
+            Console.Write("╝");
+            for (int i = 1; i < grootteX; i++)
+            {
+                Console.SetCursorPosition(i, grootteY);
+                Console.Write("═");
+            }
+
+
+        }
+
+        public void RotsenTonen() // YANNICK: Spawn rotsen op random locatie binnen gebied //This is torture
+        {
+            for (int i = 0; i < arrayrotsen.GetLength(0); i++)
+            {
+                for (int l = 0; l < arrayrotsen.GetLength(1); l++)
+                {
+                    if (arrayrotsen[i,l] == true)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        char charrots = ' ';
+                        Console.SetCursorPosition(i, l);
+                        Console.WriteLine(charrots);
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                    }
+                    
+
+                }
+            }
+        }
+
+        public void RotsenGenereren()
+        {
+            
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrayrotsen[posRoverRotsX = random.Next(1, 39),posRoverRotsY = random.Next(1, 19)] = true;
+                
+            }
+            
+           
+
+        }
+    }
+
+        class GenerateWater
+        {
+            int Xmax;
+            int Ymax;
+            int kans;
+            bool[,] waterplaatsen;
+            Random Generate = new Random();
+
+            public GenerateWater(int x, int y)
+            {
+
+                waterplaatsen = new bool[x, y];
+                Xmax = x;
+                Ymax = y;
+            }
+
+            public void Plaats()
+            {
+                for (int i = 1; i < Xmax - 1; i++)
+                {
+                    for (int j = 1; j < Ymax - 1; j++)
+                    {
+                        kans = Generate.Next(0, 10);
+                        if (kans == 1)
+                        {
+                            waterplaatsen[i, j] = true;
+                        }
+                        else { waterplaatsen[i, j] = false; }
+                    }
+                }
+            }
+            public void WaterZien()
+            {
+                for (int i = 1; i < Xmax - 1; i++)
+                {
+                    for (int j = 1; j < Ymax - 1; j++)
+                    {
+                        if (waterplaatsen[i, j] == true)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.SetCursorPosition(i, j);
+                            Console.Write("%");
+                        }
+
+                    }
+                }
+            }
+            public void WaterNietZien()
+            {
+                for (int i = 1; i < Xmax - 1; i++)
+                {
+                    for (int j = 1; j < Ymax - 1; j++)
+                    {
+                        if (waterplaatsen[i, j] == true)
+                        {
+                            Console.Write(" ");
+                        }
+
+                    }
+                }
+            }
+        }
+    
+}
---
+++ MarsRover/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Program
+    {
+ 
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed; // mars :-)
+            Console.Clear();
+            Console.CursorVisible = false; // cursor weg
+            
+            //Barier grens = new Barier();
+
+            Mars mars = new Mars();
+            Basisstation station = new Basisstation();
+            Energie energie = new Energie();
+            InSight rover = new InSight();
+            GenerateWater water = new GenerateWater(mars.grootteX, mars.grootteY);
+            rover.ToonInSight();
+            mars.toonMars();
+
+            //grens.test(rover);
+            station.toonBasis();
+            water.Plaats();
+
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var command = Console.ReadKey().Key;
+
+                    switch (command)
+                    {
+                        case ConsoleKey.DownArrow: //naar benedenbewegen
+                            rover.moveDown();
+                            break;
+                        case ConsoleKey.UpArrow:
+                            rover.moveUp();
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            rover.moveLeft();
+                            break;
+                        case ConsoleKey.RightArrow:
+                            rover.moveRight();
+                            break;
+                        case ConsoleKey.T:
+                            water.WaterZien();
+                            break;
+                        case ConsoleKey.Y:
+                            water.WaterNietZien();
+                            break;
+                        case ConsoleKey.Enter:
+                            rover.boor();
+                            break;
+                        case energie.afgaanOverrots
+                    }
+                    Console.Clear();
+                    rover.ToonInSight();
+                    mars.toonMars();
+
+                    mars.RotsenTonen();
+
+
+                    station.toonBasis();
+
+                }
+            }
+        }
+    }
+   
+
+    class InSight
+    {
+        //hey kids
+        char symbool = '#';
+        ConsoleColor kleur = ConsoleColor.Yellow;
+
+        public int posX = 1;
+        public int posY = 1;
+        Energie F;
+        //verbreuk per verplaatsing
+        public int vpv = 1;
+
+        public InSight()
+        {
+             F = new Energie();
+        }
+
+        public InSight(char symbool, ConsoleColor kleur)
+        {
+            this.symbool = symbool;
+            this.kleur = kleur;
+        }
+
+        public void moveUp()
+        {
+
+            if (posY > 1 && F.huidigverbruik() > 0)
+            {
+                posY--;
+                F.verbruik(vpv);
+            }
+        }
+
+        public void moveDown()
+        {
+  
+            if (posY < 19 && F.huidigverbruik() > 0)
+            {
+              posY++;
+              F.verbruik(vpv);
+            }
+        }
+
+        public void moveLeft()
+        {
+
+            if (posX > 1 && F.huidigverbruik() > 0)
+            {
+                posX--;
+                F.verbruik(vpv);
+            }
+       
+     
+        }
+
+        public void moveRight()
+        {
+
+            if (posX < 39 && F.huidigverbruik() > 0)
+            {
+              posX++;
+              F.verbruik(vpv);
+            }
+        }
+        
+
+        public void ToonInSight()
+        {
+            if (posX >= 0 && posY >= 0) 
+            {
+                Console.ForegroundColor = kleur;
+                Console.SetCursorPosition(posX, posY);
+                Console.Write(symbool);
+            }
+        }
+        //boren
+        bool succes = false;
+        char waterplas = '〰';
+        
+        ConsoleColor water = ConsoleColor.Blue;
+        public void boor()
+        {
+            //for (int i =0; i < .length; i++)
+            //{
+
+            //}
+        }
+        public void toonwater(bool succes)
+        {
+            if (succes == true)
+            {
+                Console.SetCursorPosition(posX, posY);
+                Console.ForegroundColor = water;
+                Console.Write(waterplas);
+            }
+            else
+            {
+
+            }
+        }
+
+    }
+
+    class Energie
+    {
+        Mars mars = new Mars();
+        InSight insight = new InSight();
+
+        
+        private int fuel = 50;
+        public int verbruik(int F) {
+            fuel = fuel - F;
+            return fuel;
+        }
+        public int huidigverbruik()
+        {
+            return fuel;
+        }
+        public void opladen()
+        {
+            fuel = 50;
+
+        }
+        public int afgaanOverrots(int fuel)
+        {
+            if(mars.posRoverRotsX == insight.posX && mars.posRoverRotsY == insight.posY)
+            {
+                fuel = fuel - 5; 
+            }
+            return fuel;
+
+    }
+
+    class Basisstation
+    {
+        char symbool = '▀';
+        ConsoleColor basis = ConsoleColor.Green;
+        int posX = 5;
+        int posY = 3;
+
+        public void toonBasis()
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.Write(symbool);
+        }
+
+
+        private void Laadstation()
+        {
+            if (posX == 50 && posY == 30)
+            {
+                Opladen();
+            }
+        }
+
+        private void Opladen()
+        {
+            //energie = energie++;
+        }
+
+}
+
+}
+
+
